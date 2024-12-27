02371233c4eacda21dc4176ab15348fc073cb696
Dalbirsm03 <dalbirsm03@gmail.com>
2024-12-28 00:10:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ def get_gemini_response(input_prompt , image , input):
     return response.text
 
 #Streamlit Setup
-st.set_page_config(page_title = "Multilanguage Invoice Extractor")
+st.set_page_config(page_title = "Multilanguage Image Extractor")
 st.header("Gemini Apllixation")
 input = st.text_input("Input Prompt" , key = 'input')#Ask the question
 upload_file = st.file_uploader("Choose Image ..." , type= ['jpg' , 'png' , 'jpeg'])#To upload image
